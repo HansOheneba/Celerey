6225769f648ac288a8525bac8be65989b8343e3c
Hans Opoku <88454205+HansOheneba@users.noreply.github.com>
2025-02-13 07:26:09 +0000

    resident country removed

--- src/Features/onboarding/types.ts
+++ src/Features/onboarding/types.ts
@@ -88,8 +88,6 @@ export interface BioDataScreenProps {
       year: string;
     };
     citizenship: string;
-    residentCountry: string;
-    dualCitizenship?: string;
   };
   onChange: (value: BioDataScreenProps["value"]) => void;
 
